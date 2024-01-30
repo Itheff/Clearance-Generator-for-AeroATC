1018dac91ba85d9d899468de4a8ba013c54c1bc6
Itheff <bnagyva@gmail.com>
2024-01-30 11:42:57 -0500

    V0.3.0
    Added findFrequency method for cleaner code and expandability
    Added automatic frequency locating for PDC and oral non-SID clearances

--- src/main/java/FlightPlan.java
+++ src/main/java/FlightPlan.java
@@ -43,9 +43,76 @@ public class FlightPlan {
         Random rand = new Random();
         int min = 0;
         int max = 4096;
-        int randomNumberInRange = rand.nextInt(max - min) + min;
-        String octalNumber = String.format("%04o",randomNumberInRange);
-        return octalNumber;
+        int decimalNum = rand.nextInt(max - min) + min;
+        String squawk = String.format("%04o",decimalNum);
+        return squawk;
+    }
+
+    /**
+     * DEL: 0
+     * GND: 1
+     * TWR: 2
+     * APP: 3
+     * DEP: 4
+     * CTR: 5
+     * @param frequencyType integer representing the type of frequency being searched for, see above
+     * @param icao string representing the ICAO code of the airport where frequencies are being checked
+     * @return a string containing the requested frequency OR the lowest frequency found above the requested frequency
+     */
+    public String findFreqency(String icao, int frequencyType) {
+
+        // This makes a BufferedReader object which is read by the loop below
+        InputStream inputStream = FlightPlan.class.getResourceAsStream("/resources/airport_database.csv");
+        InputStreamReader streamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+
+        // For some reason the whole thing crashes if it's not in a try block
+        try {
+            // This checks all the ICAO codes in the aircraft database against the departure ICAO code in the flight plan
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] splitLine = line.split(",");
+                if (splitLine[0].equals(flightPlan[6])) {
+                    // When a matching ICAO code is found, we begin to search for the lowest frequency at or above the one specified
+                    if (frequencyType == 0) {
+                        for (int i = 2; i < 8; ++i) {
+                            if (!splitLine[i].equals("None")) return splitLine[i];
+                        }
+                    }
+                    else if (frequencyType == 1) {
+                        for (int i = 3; i < 8; ++i) {
+                            if (!splitLine[i].equals("None")) return splitLine[i];
+                        }
+                    }
+                    else if (frequencyType == 2) {
+                        for (int i = 4; i < 8; ++i) {
+                            if (!splitLine[i].equals("None")) return splitLine[i];
+                        }
+                    }
+                    else if (frequencyType == 3) {
+                        for (int i = 5; i < 8; ++i) {
+                            if (!splitLine[i].equals("None")) return splitLine[i];
+                        }
+                    }
+                    else if (frequencyType == 4) {
+                        for (int i = 6; i < 8; ++i) {
+                            if (!splitLine[i].equals("None")) {return splitLine[i];}
+                        }
+                    }
+                    else if (frequencyType == 5 && !splitLine[7].equals("None")) return splitLine[7];
+
+                    else break;
+                }
+            }
+    
+            // Honestly, I don't know why this try block is here, this never raises any exceptions so it pretty useless
+            // But it doesn't work otherwise, ask ChatGPT I guess because I did, and this is what it spat out and it worked
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "N/A";
+        
     }
 
     /**
@@ -69,35 +136,7 @@ public class FlightPlan {
         clearanceBuilder.append(" transition, then as filed. Climb via the SID and expect FL");
         clearanceBuilder.append(flightPlan[9]);
         clearanceBuilder.append(" 10 minutes after departure. Departure frequency on ");
-        // This makes a BufferedReader object which is read by the loop below
-        InputStream inputStream = FlightPlan.class.getResourceAsStream("/resources/airport_database.csv");
-        InputStreamReader streamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(streamReader);
-        String frequency = "N/A";
-
-        // For some reason the whole thing crashes if it's not in a try block
-        try {
-        // This checks all the ICAO codes in the aircraft database against the departure ICAO code in the flight plan
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            String[] splitLine = line.split(",");
-            if (splitLine[0].equals(flightPlan[6])) {
-                // When a matching ICAO code is found, we then check if there is a departure frequency in the database
-                // If there isn't, then we use the center frequency for departure
-                if (splitLine[6].equals("None")) frequency = splitLine[7];
-                else frequency = splitLine[6];
-
-            }
-
-        }
-
-        // Honestly, I don't know why this try block is here, this never raises any exceptions so it pretty useless
-        // But it doesn't work otherwise, ask ChatGPT I guess because I did, and this is what it spat out and it worked
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        clearanceBuilder.append(frequency);
+        clearanceBuilder.append(findFreqency(flightPlan[6], 4));
         clearanceBuilder.append(", squawk ");
         clearanceBuilder.append(genSquawk());
         clearanceBuilder.append(".");
@@ -122,8 +161,9 @@ public class FlightPlan {
         // FIXME: Allow for different altitudes and times and perhaps allow for route readback
         clearanceBuilder.append(" as filed. Initial altitude 4000, expect FL");
         clearanceBuilder.append(flightPlan[9]);
-        // FIXME: Frequencies need implementing
-        clearanceBuilder.append(" 10 minutes after departure. Departure frequency on N/A, squawk ");
+        clearanceBuilder.append(" 10 minutes after departure. Departure frequency on ");
+        clearanceBuilder.append(findFreqency(flightPlan[6], 4));
+        clearanceBuilder.append(", squawk ");
         clearanceBuilder.append(genSquawk());
         clearanceBuilder.append(".");
 
@@ -156,8 +196,9 @@ public class FlightPlan {
         clearanceBuilder.append(flightPlan[8].substring(7,12));
         clearanceBuilder.append("TRANSITION, CLIMB VIA SID EXP ");
         clearanceBuilder.append(flightPlan[9]);
-        // FIXME: Frequencies need implementing
-        clearanceBuilder.append(" 10 MINS AFTER DPT, DPT FREQ N/A, SQUAWK ");
+        clearanceBuilder.append(" 10 MINS AFTER DPT DPT FREQ ");
+        clearanceBuilder.append(findFreqency(flightPlan[6], 4));
+        clearanceBuilder.append(", SQUAWK ");
         clearanceBuilder.append(genSquawk());
         clearanceBuilder.append(".");
 
